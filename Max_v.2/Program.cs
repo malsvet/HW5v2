dbf0124913c00d1755a4a6756a06788b3cb74166
malsvet <47292391+malsvet@users.noreply.github.com>
2022-10-19 00:56:23 +0700

    Add files via upload

---
+++ Max_v.2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа запрашивает у пользователя три числа и выводит на экран максимальное число.
+
+double[] Array;
+Array = new double[3];
+int i = 0;
+Console.Write($"Введите {i + 1} число: ");
+Array[i] = Double.Parse(Console.ReadLine());
+i++;
+Console.Write($"Введите {i + 1} число: ");
+Array[i] = Double.Parse(Console.ReadLine());
+i++;
+Console.Write($"Введите {i + 1} число: ");
+Array[i] = Double.Parse(Console.ReadLine());
+double max_array = Array.Max();
+Console.WriteLine("Максимальное число: " + max_array);
+Console.ReadKey();
